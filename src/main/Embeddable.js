61b1f70ec1c50e4a50e8f4374069ae0b5d6bcedc
mroloux <matti.roloux@gmail.com>
2021-03-02 10:16:25 +0100

    chart.js is reloaded when the chartJsUrl property changes (#15)
    Co-authored-by: mroloux <matti@seats.io>

--- src/main/Embeddable.js
+++ src/main/Embeddable.js
@@ -1,71 +1,78 @@
 /*global seatsio*/
 
-import React from 'react';
-import { didPropsChange } from './util';
+import React from 'react'
+import {didPropsChange} from './util'
 
 export default class Embeddable extends React.Component {
-    async componentDidMount() {
-        this.createAndRenderChart();
+    async componentDidMount () {
+        this.createAndRenderChart()
     }
 
-    async componentDidUpdate(prevProps) {
+    async componentDidUpdate (prevProps) {
         if (didPropsChange(this.props, prevProps) && this.chart) {
-            this.destroyChart();
+            this.destroyChart()
             this.createAndRenderChart()
         }
     }
 
-    async createAndRenderChart() {
-        const seatsio = await this.getSeatsio();
-        const config = this.extractConfigFromProps();
-        config.divId = this.props.id;
-        this.chart = this.createChart(seatsio, config).render();
+    async createAndRenderChart () {
+        const seatsio = await this.getSeatsio()
+        const config = this.extractConfigFromProps()
+        config.divId = this.props.id
+        this.chart = this.createChart(seatsio, config).render()
         if (this.props.onRenderStarted) {
-            this.props.onRenderStarted(this.chart);
+            this.props.onRenderStarted(this.chart)
         }
     }
 
-    extractConfigFromProps() {
+    extractConfigFromProps () {
         // noinspection JSUnusedLocalSymbols
-        let {id, className, onRenderStarted, chartJsUrl, ...config} = this.props;
-        return config;
+        let { id, className, onRenderStarted, chartJsUrl, ...config } = this.props
+        return config
     }
 
-    componentWillUnmount() {
+    componentWillUnmount () {
         this.destroyChart()
     }
 
-    destroyChart() {
+    destroyChart () {
         if (this.chart && this.chart.state !== 'DESTROYED') {
-            this.chart.destroy();
+            this.chart.destroy()
         }
     }
 
-    getSeatsio() {
-        if (typeof seatsio !== 'undefined') {
-            return Promise.resolve(seatsio);
+    getSeatsio () {
+        if (typeof seatsio === 'undefined') {
+            return this.loadSeatsio()
+        } else if (seatsio.chartJsUrl !== this.props.chartJsUrl) {
+            seatsio = undefined
+            return this.loadSeatsio()
+        } else {
+            return Promise.resolve(seatsio)
         }
-        return this.loadSeatsio();
     }
 
-    loadSeatsio() {
+    loadSeatsio () {
         return new Promise((resolve, reject) => {
-            let script = document.createElement('script');
-            script.onload = () => resolve(seatsio);
-            script.onerror = () => reject(`Could not load ${script.src}`);
-            script.src = this.props.chartJsUrl;
-            document.head.appendChild(script);
-        });
+            let script = document.createElement('script')
+            script.onload = () => {
+                seatsio.chartJsUrl = this.props.chartJsUrl
+                resolve(seatsio)
+            }
+            script.onerror = () => reject(`Could not load ${script.src}`)
+            script.src = this.props.chartJsUrl
+            document.head.appendChild(script)
+        })
     }
 
-    render() {
+    render () {
         return (
             <div id={this.props.id} className={this.props.className}/>
-        );
+        )
     }
 }
 
 Embeddable.defaultProps = {
     id: 'chart',
     chartJsUrl: 'https://cdn.seatsio.net/chart.js'
-};
+}

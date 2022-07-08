4e27ed1579adabeb56b4a71273603c339dabfb2a
Matti Roloux <matti@seats.io>
2022-07-08 10:16:11 +0200

    Removed babel-polyfill and regenerator-runtime, since they're only needed for old browsers

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
 module.exports = {
-    entry: ['babel-polyfill', './src/main/index.js'],
+    entry: ['./src/main/index.js'],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.js',
@@ -12,10 +12,7 @@ module.exports = {
                 test: /\.js$/,
                 include: path.resolve(__dirname, 'src'),
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
+                    loader: 'babel-loader'
                 }
             }
         ]

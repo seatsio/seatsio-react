33bf52ca8f989c7748b643e512f1694a788127c6
Morten Devold <morten@seats.io>
2023-09-22 15:06:03 +0200

    Simplify jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  "testMatch": [
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
   testEnvironment: "jsdom"
 }

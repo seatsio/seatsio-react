ebf4ad83dd798e27075adef9368b8031d4b09c67
Morten Devold <morten@seats.io>
2023-10-04 10:27:18 +0200

    Delete chart manager

--- src/main/SeatsioChartManager.tsx
+++
@@ -1,9 +0,0 @@
-import { EventManagerConfigOptions } from '@seatsio/seatsio-types'
-import Embeddable from './Embeddable'
-
-
-export default class SeatsioChartManager extends Embeddable<EventManagerConfigOptions> {
-    createChart <EventManagerConfigOptions>(seatsio: any, config: EventManagerConfigOptions) {
-        return new seatsio.ChartManager(config)
-    }
-}

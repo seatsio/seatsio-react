1cc893268779ec055667b32f66f9f0896a098ebe
Morten Devold <morten@seats.io>
2023-10-20 12:36:38 +0200

    Convert playground app to TypeScript

--- playground/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.css';
-import App from './App';
-import {createRoot} from "react-dom/client";
-
-const container = document.getElementById('root');
-const root = createRoot(container)
-root.render(<App />);

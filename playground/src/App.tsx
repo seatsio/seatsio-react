ebca41e4cfbc5d6a94c97bf9bd6741a3c4dc2846
Morten Devold <morten@seats.io>
2023-10-20 13:15:33 +0200

    Add dark mode

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -9,25 +9,27 @@ export const App = () => {
   const [colorScheme, setColorScheme] = useState<ColorScheme>('light')
 
   return (
-    <div className="App">
-        <select onChange={e => setColorScheme(e.target.value as ColorScheme)} value={colorScheme}>
-            <option value="light">Light</option>
-            <option value="dark">Dark</option>
-        </select>
-        <select onChange={e => setUnusedState(parseInt(e.target.value))} value={unusedState}>
-            <option>0</option>
-            <option>1</option>
-        </select>
-        <h1>Seats.io React playground</h1>
-        <div id="chart">
-            { /* @ts-ignore */ }
-            <SeatsioSeatingChart
-                workspaceKey="publicDemoKey"
-                event="smallTheatreEvent1"
-                colorScheme={colorScheme}
-                region="eu"
-                chartJsUrl="https://cdn-staging-{region}.seatsio.net/chart.js"
-            />
+    <div className={['container', colorScheme].join(' ')}>
+        <div className="App">
+            <select onChange={e => setColorScheme(e.target.value as ColorScheme)} value={colorScheme}>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+            </select>
+            <select onChange={e => setUnusedState(parseInt(e.target.value))} value={unusedState}>
+                <option>0</option>
+                <option>1</option>
+            </select>
+            <h1>Seats.io React playground</h1>
+            <div id="chart">
+                { /* @ts-ignore */ }
+                <SeatsioSeatingChart
+                    workspaceKey="publicDemoKey"
+                    event="smallTheatreEvent1"
+                    colorScheme={colorScheme}
+                    region="eu"
+                    chartJsUrl="https://cdn-staging-{region}.seatsio.net/chart.js"
+                />
+            </div>
         </div>
     </div>
   )

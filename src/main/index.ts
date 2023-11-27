b323b27299f7495405ea2a2a2daa7f16116cfc1d
mroloux <matti@seats.io>
2023-11-27 14:46:06 +0100

    Don't unnecessarily export seatsio-types (#149)

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,4 +2,3 @@ export { default as SeatsioDesigner } from './SeatsioDesigner'
 export { default as SeatsioEventManager } from './SeatsioEventManager'
 export { default as SeatsioSeatingChart } from './SeatsioSeatingChart'
 export { isBooth, isGeneralAdmission, isSeat, isSection, isTable } from './util'
-export type * from '@seatsio/seatsio-types';

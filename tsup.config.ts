16430e6c13efee9bf03faf9f935e16e5ab6824b0
csandman <chris@getcuebox.com>
2024-06-28 17:39:52 -0400

    Fix the build config to only include necessary files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/main"],
+  entry: ["src/main/index.ts"],
   outDir: "build",
   format: ["cjs", "esm"],
   target: "es2019",
   sourcemap: true,
   dts: true,
-  splitting: false,
+  clean: true,
 });
